ba3df98e52df035f3d2a216ce127f241c46dffa3
livein21st <niravpmalsattar@gmail.com>
2019-11-06 17:01:54 +0100

    Revert "check"
    This reverts commit 31810089a2abe58730d14d9a36f84e6217078c02.

--- example/Arduino_nano33_MQTT/arduino_secrets.h
+++ example/Arduino_nano33_MQTT/arduino_secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "iPhone"
-#define SECRET_PASS "sambharo1234$"
+#define SECRET_SSID "YOUR WIFI/HOTSPOT SSID"
+#define SECRET_PASS "PASSWORD"
 
 #define THING_ID "YOUR THING ID"
 #define THING_TOKEN "YOUR THING TOKEN"
